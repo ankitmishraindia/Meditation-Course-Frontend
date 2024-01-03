b6b8ff38b9e29c1febf1230d6e30e87e8a3aaafe
ankit mishra <mishraankit987@gmail.com>
2024-01-03 13:57:23 +0530

    Added course part in page

--- README.md
+++ README.md
@@ -1,36 +1,42 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Meditation Course Page(Nextjs)
 
-## Getting Started
+This is a course page(frontend) for meditators, who want to learn specific spiritual techniques.
 
-First, run the development server:
+### How to use
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+1. Clone this project into your system.
 ```
+  git clone https://github.com/ankitmishraindia/Meditation-Course-Frontend
+```
+2. Change directory.
+```
+  cd ./Meditation-Course-Frontend
+```
+3. Install dependencies 
+```
+  npm install
+```
+4. Run the server
+```
+  npm run dev
+```
+5. Now you can change your page.js file in src/app  for desired output.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+### How to create
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+1. Run create-next-app command
+```
+  npx create-next-app@latest
+```
+2. Write your project name and follow commands accordingly.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+3. Change directory.
+```
+  cd ./Meditation-Course-Frontend
+```
+4. Run the server
+```
+  npm run dev
+```
+5. Now you can change your page.js file in src/app  for desired output.
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,85 @@
+import { document } from 'postcss'
 import Data from '../Data/pageData.json'
+import Image from 'next/image'
+import { FaRupeeSign } from "react-icons/fa";
+import { MdOndemandVideo } from "react-icons/md";
+import { RiQuestionAnswerLine } from "react-icons/ri";
 
 export default function Home() {
 
-  const {instructor}=Data
+  const {
+         instructor,
+         course,
+         about_instructor,
+         testimonial
+        }=Data
+
   return (
-    <h1 className="text-3xl text-red-400">{instructor.name}</h1>
+    <div className='w-full space-y-0'>
+
+        <header className='w-full h-[500px] relative'>
+           <Image src='/header-image.jpeg' layout='fill' objectFit='cover' alt='header-image'/>
+           <div className='absolute top-0 left-0 w-full h-full bg-[linear-gradient(90deg,rgba(0,0,0,0.8),rgba(0,0,0,0.1))]'></div>
+           <div className='absolute left-32 bottom-10  w-1/2 space-y-1'>
+              <h3 className='text-3xl font-bold text-gray-300'>{instructor.name}</h3>
+              <h1 className='text-5xl font-bold text-white'>{course.title}</h1>
+           </div>
+        </header>
+
+        <main className='w-full relative space-y-7'>
+
+           <div className='w-[60%] pl-[15%] space-y-5'>
+               <div className='flex w-[80%]'>
+                 <div className='py-2 px-10 font-semibold text-xl border-b-4 border-b-violet-600 cursor-pointer'>ABOUT</div>
+                 <div className='py-2 px-10 font-semibold text-xl cursor-pointer'>INSTRUCTOR</div>
+                 <div className='py-2 px-10 font-semibold text-xl cursor-pointer'>REVIEWS</div>
+               </div>
+               <h2 className='text-3xl font-bold'>About the course</h2>
+               <div className='space-y-4 text-xl' dangerouslySetInnerHTML={{__html:course.about.html_content}}>
+               </div>
+               <h2 className='text-3xl font-semibold'>What to expect from the course</h2>
+               <div className='text-xl' dangerouslySetInnerHTML={{__html:course.what_to_expect.html_content}}></div>
+               <h2 className='text-3xl font-semibold'>Key topics covered</h2>
+               <div className='space-y-2'>
+                  <div className='text-xl' dangerouslySetInnerHTML={{__html:course.key_topics.html_content}}></div>
+               </div>
+           </div>
+
+           <div className='absolute w-[280px] h-[300px] bg-violet-50 right-40 -top-20 p-5 flex flex-col justify-between'>
+               <div>
+                  <p className='font-bold'>Course fees</p>
+                  <p className='flex'>
+                     <span>{(course.fee.currency==='INR')?<FaRupeeSign size={22}/>:course.fee.currency}</span>
+                     <span className='text-2xl font-semibold font-serif'>5,000</span>
+                  </p>
+               </div>
+               <div className='space-y-2'>
+                  <p className='font-semibold'>What's included:</p>
+                  <ul className='text-sm space-y-1 list-none'>
+                     {course.inclusions.on_demand_videos&&(<li className='flex items-center gap-2'><MdOndemandVideo/>
+                     <span >{course.inclusions.on_demand_videos} on-demand videos.</span>
+                     </li>)}
+                     {course.inclusions.live_qa_sessions&&(<li className='flex items-center gap-2'><RiQuestionAnswerLine/>
+                     <span >Live QA sessions.</span>
+                     </li>)}
+                     {course.inclusions.whatsapp_community&&(<li className='flex items-center gap-2'><RiQuestionAnswerLine/>
+                     <span >An active whatsapp community.</span>
+                     </li>)}
+                  </ul>
+               </div>
+               <button className='bg-violet-800 text-white py-2 w-full rounded-2xl text-sm font-bold'>Register Today</button>
+           </div>
+
+           <div className='space-y-2 w-[94%] pl-[15%] pt-8'>
+               <h2 className='text-3xl font-bold'>About the Instructor</h2>
+               <div>
+                  <div className='text-sm rounded-full w-[100px] h-[100px] overflow-hidden relative'>
+                     <Image src='/instructor-image.jpeg' layout='fill' objectFit='cover' alt='instructor-image'/>
+                  </div>
+                  
+               </div>
+           </div>
+        </main>
+    </div>
   )
 }
